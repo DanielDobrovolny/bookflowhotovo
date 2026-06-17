844cf1728797da52f9157c96ac737ed0cade5659
Daniel Dobrovolný <D23253@oa-opava.czgit config --global user.email D23253@oa-opava.cz>
2026-06-17 20:36:06 +0200

    test

--- bookflow/settings.py
+++ bookflow/settings.py
@@ -57,12 +57,8 @@ WSGI_APPLICATION = 'bookflow.wsgi.application'
 # Standardní konfigurace databáze pro MySQL / MariaDB
 DATABASES = {
     'default': {
-        'ENGINE': 'bookflow.mysql_compat',  # <--- TADY změníme engine na náš upravený
-        'NAME': 'bookflow_clean',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
